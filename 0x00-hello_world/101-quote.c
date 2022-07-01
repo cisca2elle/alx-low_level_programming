935ab4db537fcb582ab5cf3547593462633f0035
cisca2elle <cisca2elle@gmail.com>
2022-07-01 04:08:05 -0700

    C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - Prints out the last part of a quote in the standard error.
- * Return: 1 if success.
+ * Return: 1 if success
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, ch, sizeof(ch));
 	return (1);
 }
