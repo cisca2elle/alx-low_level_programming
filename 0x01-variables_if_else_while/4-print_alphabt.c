a9f5c2c4a5f82b8a03a1dad55879dc089860087e
cisca2elle <cisca2elle@gmail.com>
2022-07-04 18:17:33 -0700

    CP

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: 0
+*/
+int main(void)
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+{
+	if (c != 'q' && c != 'e')
+	putchar(c);
+	c++;
+}
+	putchar('\n');
+	return (0);
+}
